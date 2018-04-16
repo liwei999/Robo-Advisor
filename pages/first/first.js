3d74de3bb625e0198c07da7900ad3f5dbcba67a5
dsfofwuc <2570626121@qq.com>
2018-04-16 16:59:30 +0800

    添加事件
    添加事件

--- pages/first/first.js
+++ pages/first/first.js
@@ -5,7 +5,11 @@ Page({
    * 页面的初始数据
    */
   data: {
-    imgUrls: ["../../image/banner.png", "../../image/banner2.png", "../../image/banner3.png"]
+    imgUrls: ["../../image/banner.png", "../../image/banner2.png", "../../image/banner3.png"],
+    desire: [{ title: '两年后的旅行', money: '10000', time: 6 }, { title: '六年后的培养', money: '20000', time: 72 }, { title: '存1万块', money: '10000', time: 12 }, { title: '买个LV包', money: '20000', time: 9 },{ title: '买个MacBook', money: '6000', time: 8 }],
+    desireInp:"",
+    desireMoney:"",
+    desiretime:"6",
   },
 
   /**
@@ -67,8 +71,52 @@ Page({
    * 下一步
    */
   next_step:function(e){
+    
+    var that=this;
+    if (!this.data.desireInp)
+    {
+      wx.showToast({
+        title: '请输入愿望',
+        icon:'none',
+        duration:2000
+      });
+      return;
+    }
+    if (!this.data.desireMoney)
+    {
+      wx.showToast({
+        title: '请输入金额',
+        icon: 'none',
+        duration: 2000
+      });
+      return;
+    }
+
     wx.navigateTo({
-      url: '../second/second',
+      url: '../second/second?title=' + this.data.desireInp + "&time=" + this.data.desiretime + "&money=" + this.data.desireMoney,
     })
+  },
+  /**
+   * 目标点击事件
+   */
+  click_desire:function(e){
+    console.log(e);
+    var that= this;
+    that.setData({ desireInp: e.currentTarget.dataset.title, desireMoney: e.currentTarget.dataset.money, desiretime: e.currentTarget.dataset.time});
+  }
+  ,
+  /**
+   * 输入愿望事件
+   */
+  desireInp:function(e){
+    this.setData({ desireInp: e.detail.value });
+  }
+  ,
+  /**
+   * 输入金额
+   */
+  desireInpMoney:function(e)
+  {
+    this.setData({ desireMoney: e.detail.value});
   }
 })
--- pages/second/second.js
+++ pages/second/second.js
@@ -5,14 +5,27 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+    timevalue:6,//实现愿望期限
+    riskvalue:6,//最大风险承受
+    typeLeft:"",//有无投入资金左
+    typeReight: "havemoneyactionr",//有无投入资金右
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    var that=this;
+    console.log(options.time);
+    if (options.time)
+    {
+    that.setData({timevalue:options.time});
+    }
+    else
+    {
+      that.setData({ timevalue: 6 });
+    }
+    
   },
 
   /**
@@ -68,5 +81,49 @@ Page({
     wx.navigateTo({
       url: '../webviewThird/webviewThird',
     })
+  },
+  /**
+   * 绑定有无投入资金
+   */
+  bindstyle:function(e){
+    var that=this;
+    if (e.currentTarget.dataset.type=="l")
+    {
+      that.setData({typeLeft: "havemoneyactionl",typeReight:""});
+    }
+    else{
+      that.setData({ typeLeft: "", typeReight: "havemoneyactionr" });
+    }
+  },
+  /**
+   * 绑定实现愿望和风险承受减号
+   */
+  timereduce:function(e){
+    var that = this;
+    if (e.currentTarget.dataset.time=="one")
+    {
+      var time = that.data.timevalue > 0 ? --that.data.timevalue: 0;
+      that.setData({ timevalue: that.data.timevalue});      
+    }
+    else
+    {
+      console.log(that.data.riskvalue)
+      var risk = that.data.riskvalue > 0 ? --that.data.riskvalue : 0;
+      that.setData({ riskvalue: that.data.riskvalue });      
+    }
+  }
+  ,
+  /**
+   * 绑定实现愿望和风险承受加号
+   */
+  timeadd:function(e){
+    var that = this;
+    if (e.currentTarget.dataset.time == "one") {
+      that.setData({ timevalue: ++that.data.timevalue });
+    }
+    else
+    {
+      that.setData({ riskvalue: ++that.data.riskvalue });      
+    }
   }
 })
